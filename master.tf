a02b4d8b3f45bb8ca3c546bbab2759187f17a3c8
alessanderviana <alessander.viana@hotmail.com>
2020-03-08 19:19:50 -0300

    Added labels in startup script

--- master.tf
+++ master.tf
@@ -26,30 +26,30 @@ resource "google_compute_instance" "kube-master" {
  }
 
  metadata_startup_script = <<SCRIPT
-    echo "UPGRADE S.O."
+    echo "***** UPGRADE S.O. *****"
     apt-get update -y && apt-get upgrade -y
-    echo "INSTALLS DOCKER"
+    echo "***** INSTALLS DOCKER *****"
     curl -fsSL https://get.docker.com/ | bash
     usermod -aG docker ubuntu
-    echo "INSTALLS DEPENDENCIES"
+    echo "***** INSTALLS DEPENDENCIES *****"
     apt-get update && apt-get install -y apt-transport-https
-    echo "KUBERNETES REPO AND KEY"
+    echo "***** KUBERNETES REPO AND KEY *****"
     curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
     echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
-    echo "INSTALLS KUBERNETES"
+    echo "***** INSTALLS KUBERNETES *****"
     apt-get update -y && apt-get install -y kubelet kubeadm kubectl
-    echo "DOCKER CGROUP DRIVER"
+    echo "***** DOCKER CGROUP DRIVER *****"
     echo -e "{\n \"exec-opts\": [\"native.cgroupdriver=systemd\"],\n \"log-driver\": \"json-file\",\n \"log-opts\": {\n   \"max-size\": \"100m\"\n },\n \"storage-driver\": \"overlay2\",\n \"storage-opts\": [\n   \"overlay2.override_kernel_check=true\"\n ]\n}" > /etc/docker/daemon.json
     mkdir -p /etc/systemd/system/docker.service.d && systemctl daemon-reload && systemctl restart docker
-    echo "DOWNLOAD KUBERNETES IMAGES"
+    echo "***** DOWNLOAD KUBERNETES IMAGES *****"
     kubeadm config images pull
-    echo "KUBERNETES INIT"
+    echo "***** KUBERNETES INIT *****"
     kubeadm init >/home/ubuntu/kube-init.log 2>&1
-    echo "KUBERNETES CONFIG"
+    echo "***** KUBERNETES CONFIG *****"
     sudo --user=ubuntu mkdir -p /home/ubuntu/.kube
     cp -i /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
     chown ubuntu: /home/ubuntu/.kube/config
-    echo "INSTALLS WEAVE NET"
+    echo "***** INSTALLS WEAVE NET *****"
     kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 SCRIPT
 
